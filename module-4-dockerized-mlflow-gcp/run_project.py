71e32a0b678dc716569046327813342ce1a4c28e
Ryan Cohn <rcohn@andrew.cmu.edu>
2022-09-19 19:47:22 +0000

    added comments

--- module-4-dockerized-mlflow-gcp/run_project.py
+++ module-4-dockerized-mlflow-gcp/run_project.py
@@ -7,13 +7,19 @@ import sys
 
 
 def main():
-    # load environment variables, like tracking uri
+    # load environment variables, like tracking uri and data location
     load_dotenv()
+
     # set USER_UID variable to pass to container
     os.environ['USER_UID']=str(os.getuid())
+
     # need absolute path for docker bind mount
     os.environ['DATA_PATH']=str(Path(os.environ['DATA_PATH']).absolute())
+
+    # read params from command line, if there are any
     params = {x.split('=')[0]: x.split('=')[1] for x in sys.argv[1:]} 
+
+    # run the project
     mlflow.projects.run(uri='gnn-project', parameters=params)
 
 
