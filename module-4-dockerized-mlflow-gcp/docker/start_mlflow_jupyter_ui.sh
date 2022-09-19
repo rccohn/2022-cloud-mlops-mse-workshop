1f6b115c9feff746e92a445e8527b427d0c72e37
Ryan Cohn <rcohn@cmu.edu>
2022-09-19 11:03:26 -0400

    clean up and add comments

--- module-4-dockerized-mlflow-gcp/docker/start_mlflow_jupyter_ui.sh
+++ module-4-dockerized-mlflow-gcp/docker/start_mlflow_jupyter_ui.sh
@@ -1,26 +1,43 @@
 #! /bin/bash 
 # Enter storage bucket, sql connection string, postgres password, and username
-export ARTIFACT_URI=
+# name of your storage bucket
+export BUCKET=
+# SQL connection string, <project>:<zone>:<name>
 export SQL_CONNECTION=
+# password for default account on postgres
 export POSTGRES_PASSWD=
+# YOUR account (without @gmail.com), so the startup script knows which home directory to use
 export GCP_USER=
+# Set the username and password for accessing jupyter and the mlflow ui remotely
+export LOGIN_USER=
+export LOGIN_PASSWD=
 
 #### Don't change anything below here!!!
 export WORKDIR=/home/${GCP_USER}/
-# if code has not already been 
+export ARTIFACT_URI=gs://${BUCKET}/artifacts
+export USER_UID=$(id -u ${GCP_USER})
+
+# if code has not already been configured
 if [ ! -d ${WORKDIR}/2022-cloud-mlops-mse-workshop ];
 then
-    echo "cloning repository"
+    # download the repository
     cd ${WORKDIR} && git clone \
         https://github.com/rccohn/2022-cloud-mlops-mse-workshop.git
-        
+    # older version of compose does not have -f {file} argument
+    # so rename the correct file docker-compose.yaml
     cd ${WORKDIR}/2022-cloud-mlops-mse-workshop/module-4-dockerized-mlflow-gcp/docker/ && \
-        mv jupyter-mlflow-ui-compose docker-compose.yaml
+        mv jupyter-mlflow-ui-compose.yaml docker-compose.yaml
 fi
 
-
+# compose isn't installed by default, so we can use a trick to run it through docker.
+# -v /var/run/docker.sock:/var/run/docker.sock allows docker compose in the container
+# to control docker on the host
+# -e arguments simply pass environment variables from host to container
+# -v $(pwd):$(pwd) -w=$(wd) mounts current directory to container and uses it
+# as the working directory to run compose
 cd ${WORKDIR}/2022-cloud-mlops-mse-workshop/module-4-dockerized-mlflow-gcp/docker && \
     docker run --rm -v /var/run/docker.sock:/var/run/docker.sock \
         -e "ARTIFACT_URI" -e "SQL_CONNECTION" -e "POSTGRES_PASSWD" \
+	-e "LOGIN_USER" -e "LOGIN_PASSWD" -e "WORKDIR" -e "USER_UID"\
         -v $(pwd):$(pwd) -w=$(pwd) docker/compose:alpine-1.29.2 \
-        up -d
+        up 
--- module-4-dockerized-mlflow-gcp/docker/start_tracking_server.sh
+++ module-4-dockerized-mlflow-gcp/docker/start_tracking_server.sh
@@ -7,15 +7,16 @@ export GCP_USER=
 
 #### Don't change anything below here!!!
 export WORKDIR=/home/${GCP_USER}/
-# if code has not already been 
+
+# if code has not already been configured
 if [ ! -d ${WORKDIR}/2022-cloud-mlops-mse-workshop ];
 then
-    echo "cloning repository"
-    # pull code from git from Alpine container
+    # pull code from git in Docker
     sudo docker run -v ${WORKDIR}:/mnt --rm alpine:3.16.2 ash -c \
         "cd /mnt && apk add --no-cache git && git clone \
         https://github.com/rccohn/2022-cloud-mlops-mse-workshop.git"
-   
+    # older version of compose doesn't have -f file argument, so
+    # rename the appropriate config "docker-compose.yaml"
     cd ${WORKDIR}/2022-cloud-mlops-mse-workshop/module-4-dockerized-mlflow-gcp/docker/ && \
         mv mlflow-server-compose.yaml docker-compose.yaml
 fi
@@ -24,7 +25,13 @@ fi
 sudo iptables -A INPUT -p tcp --dport 5000 -j ACCEPT
 
 # COS doesn't actually have compose, so we run it through docker itself
-# start the tracking server
+# start the containers
+# -v /var/run/docker.sock:/var/run/docker.sock allows docker compose in the container
+# to control docker on the host
+# -e arguments simply pass environment variables from host to container
+# -v $(pwd):$(pwd) -w=$(wd) mounts current directory to container and uses it
+# as the working directory to run compose
+
 cd ${WORKDIR}/2022-cloud-mlops-mse-workshop/module-4-dockerized-mlflow-gcp/docker && \
     docker run --rm -v /var/run/docker.sock:/var/run/docker.sock \
         -e "ARTIFACT_URI" -e "SQL_CONNECTION" -e "POSTGRES_PASSWD" \
